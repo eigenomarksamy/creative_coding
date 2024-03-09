842b9fba3e78d9d7b449082106e3cfaa57f9f3a0
eigenomar <omarkhaledsamy20@gmail.com>
2024-03-09 09:47:18 +0100

    add shapes directory

---
+++ cc_pygame/shapes/pygame_circle.py
@@ -0,0 +1,18 @@
+import pygame
+
+class Circle:
+
+    def __init__(self, surface: pygame.SurfaceType, color: str,
+                 pos: pygame.Vector2, radius: int, width: int = 0) -> None:
+        self.surface = surface
+        self.color = color
+        self.pos = pos
+        self.radius = radius
+        self.width = width
+
+    def draw(self) -> None:
+        pygame.draw.circle(self.surface, self.color, self.pos, self.radius, self.width)
+
+def draw_circles(circles: list[Circle]) -> None:
+    for circle in circles:
+        circle.draw()
