e6828d51eb0d91380e24199f445436d6f77f954f
eigenomar <omarkhaledsamy20@gmail.com>
2024-03-09 09:47:37 +0100

    modulerise flowers

--- cc_pygame/simple_flowers.py
+++ cc_pygame/simple_flowers.py
@@ -1,46 +1,74 @@
+import sys
 import random
 import pygame
+from typing import Tuple
+from shapes.pygame_circle import draw_circles, Circle
 
-class Circle:
+def draw_simple_flower(screen: pygame.SurfaceType, circles: list[Circle],
+                       center_pos_w: int, center_pos_h: int,
+                       radius: int, color: pygame.Color) -> None:
+    circles.append(Circle(screen, color, pygame.Vector2(center_pos_w, center_pos_h),
+                          radius))
+    circles.append(Circle(screen, color,
+                          pygame.Vector2(center_pos_w - radius,
+                                         center_pos_h - radius),
+                          radius))
+    circles.append(Circle(screen, color,
+                          pygame.Vector2(center_pos_w + radius,
+                                         center_pos_h + radius),
+                          radius))
+    circles.append(Circle(screen, color,
+                          pygame.Vector2(center_pos_w + radius,
+                                         center_pos_h - radius),
+                          radius))
+    circles.append(Circle(screen, color,
+                          pygame.Vector2(center_pos_w - radius,
+                                         center_pos_h + radius),
+                          radius))
+    draw_circles(circles)
 
-    def __init__(self, surface: pygame.SurfaceType, color: str,
-                 pos: pygame.Vector2, radius: int) -> None:
-        self.surface = surface
-        self.color = color
-        self.pos = pos
-        self.radius = radius
+def randomize(width_limits: tuple, height_limits: tuple,
+              colors: list[pygame.Color] = []) -> Tuple[int, int, pygame.Color]:
+    random_width = random_height = 0
+    color = None
+    if len(width_limits) == 2 and len(height_limits) == 2:
+        random_width = random.randint(width_limits[0], width_limits[1])
+        random_height = random.randint(height_limits[0], height_limits[1])
+    if len(colors) > 0:
+        color = colors[random.randint(0, len(colors) - 1)]
+    return random_width, random_height, color
 
-def draw(circles: list[Circle]) -> None:
-    for circle in circles:
-        pygame.draw.circle(circle.surface, circle.color, circle.pos, circle.radius)
+def main():
+    pygame.init()
+    res = (1280, 720)
+    max_iterations = 35
+    iterations = 0
+    radius = 50
+    colors = ["white", "crimson", "teal", "cyan", "violet", "indigo",
+              "orange", "red", "yellow", "green", "grey", "black"]
+    circles = []
+    screen = pygame.display.set_mode(res)
+    clock = pygame.time.Clock()
+    running = True
+    screen_color = colors[random.randint(0, len(colors) - 1)]
+    screen.fill(screen_color)
+    colors.remove(screen_color)
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        if iterations < max_iterations:
+            width_tuple = (radius * 1.5, screen.get_width() - radius * 1.5)
+            height_tuple = (radius * 1.5, screen.get_height() - radius * 1.5)
+            random_width, random_height, random_color = randomize(width_tuple,
+                                                                  height_tuple,
+                                                                  colors)
+            draw_simple_flower(screen, circles, random_width,
+                               random_height, radius, random_color)
+            iterations += 1
+        pygame.display.flip()
+        clock.tick(60)
+    pygame.quit()
 
-def draw_simple_flower(circles: list[Circle], center_pos_w: int, center_pos_h: int, radius: int, color: str) -> None:
-    circles.append(Circle(screen, color, pygame.Vector2(center_pos_w, center_pos_h), radius))
-    circles.append(Circle(screen, color, pygame.Vector2(center_pos_w - radius, center_pos_h - radius), radius))
-    circles.append(Circle(screen, color, pygame.Vector2(center_pos_w + radius, center_pos_h + radius), radius))
-    circles.append(Circle(screen, color, pygame.Vector2(center_pos_w + radius, center_pos_h - radius), radius))
-    circles.append(Circle(screen, color, pygame.Vector2(center_pos_w - radius, center_pos_h + radius), radius))
-    draw(circles)
-
-pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
-circles = []
-iterations = 0
-max_iterations = 35
-colors = ["white", "crimson", "teal", "cyan", "violet", "indigo", "orange", "red", "yellow", "green", "grey"]
-screen.fill("black")
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    if iterations < max_iterations:
-        random_width = random.randint(75, screen.get_width() - 75)
-        random_height = random.randint(75, screen.get_height() - 75)
-        random_color = colors[random.randint(0, len(colors) - 1)]
-        draw_simple_flower(circles, random_width, random_height, 50, random_color)
-        iterations += 1
-    pygame.display.flip()
-    clock.tick(60)
-pygame.quit()
+if __name__ == "__main__":
+    sys.exit(main())
